b57cad8a6c490681eb164590578233c67eca7fde
Ravula Prudvi <prudvirpr@gmail.com>
2023-06-15 11:15:34 +0530

    commit 1 :Day1 and Day2 activities added

---
+++ Day1/ex4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    float num1, num2, result;
+    char operand;
+
+    printf("Enter Number1: ");
+    scanf("%f", &num1);
+
+    printf("Enter the operand: ");
+    scanf(" %c", &operand);
+
+    printf("Enter Number2: ");
+    scanf("%f", &num2);
+return 0;
+}
