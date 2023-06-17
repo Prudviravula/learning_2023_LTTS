5264c0bd74a3d65d02c27a4359133acfc8647781
Ravula Prudvi <prudvirpr@gmail.com>
2023-06-17 17:59:44 +0530

    commit: updating day2_ex5.c

--- Day2/ex5.c
+++ Day2/ex5.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
-int countSetBits(unsigned int num) {
-    int count = 0;
-    while (num > 0) {
-        count += num & 1;
-        num >>= 1;
+void swapAlternate(int arr[], int size) {
+    for (int i = 0; i < size - 1; i += 2) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
     }
-    return count;
 }
 
 int main() {
-    unsigned int array[] = {0x1, 0xF4, 0x10001};
-    int size = sizeof(array) / sizeof(array[0]);
-    int totalSetBits = 0;
-    
-    for (int i = 0; i < size; i++) {
-        totalSetBits += countSetBits(array[i]);
+    int a1[] = {10, 20, 30, 40, 50};
+    int size1 = sizeof(a1) / sizeof(a1[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size1; i++) {
+        printf("%d ", a1[i]);
     }
-    
-    printf("Total number of set bits: %d\n", totalSetBits);
-    
+    printf("\n");
+
+    swapAlternate(a1, size1);
+
+    printf("Modified array: ");
+    for (int i = 0; i < size1; i++) {
+        printf("%d ", a1[i]);
+    }
+    printf("\n\n");
+
+    int a2[] = {10, 20, 30, 40, 50, 60};
+    int size2 = sizeof(a2) / sizeof(a2[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size2; i++) {
+        printf("%d ", a2[i]);
+    }
+    printf("\n");
+
+    swapAlternate(a2, size2);
+
+    printf("Modified array: ");
+    for (int i = 0; i < size2; i++) {
+        printf("%d ", a2[i]);
+    }
+    printf("\n");
+
     return 0;
 }
