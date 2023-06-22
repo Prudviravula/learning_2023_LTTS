175ce511b5717b970d5a808ddae0eacd021f13a9
Ravula Prudvi <prudvirpr@gmail.com>
2023-06-22 12:28:28 +0530

    ..

---
+++ Day1/level1/ex1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()  
+{  
+    int a, b, big;  
+  
+    printf("Enter 2 numbers\n");  
+    scanf("%d %d", &a, &b);  
+  
+    (a > b) ? (big = a) : (big = b);  
+  
+    printf("Biggest of %d and %d is %d\n", a, b, big);  
+  
+    return 0;  
+}
+/*using if else if statements */  
+int partb() {  
+    int a, b;  
+    printf("Please Enter Two different values\n");  
+    scanf("%d %d", &a, &b);  
+    
+    if(a > b) 
+    {
+        printf("%d is Largest\n", a);          
+    } 
+    else 
+    { 
+        printf("%d is Largest\n", b);  
+    } 
+    
+   
+    return 0;  
+}
+
